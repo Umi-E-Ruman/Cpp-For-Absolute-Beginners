30e34284141652308fc2923a5ba283bb71fe5699
Umi E Ruman <ummeroman488@gmail.com>
2026-08-27 03:28:26 -0700

    Added first program Hello World

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{     
+    cout<<"My name is Umi E Ruman\n";
+    cout<<"I am from Lahore";
+    cout<<"Coding is so easy";
+    cout<<"Hello World";
+    return 0;
+} 
+
+
