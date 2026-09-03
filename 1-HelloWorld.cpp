8309f9e23f030e59d1dd68e4fb06b09833460cf8
Umi E Ruman <ummeroman488@gmail.com>
2026-09-02 18:32:20 -0700

    Add: Lecture 01 - Hello World with example and comments

---
+++ 1-HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{     
+   // cout is an object and it's job is to print 
+    cout<<"Hello World";
+   //  Example we just change inner text and output changed
+     cout<<"Umi E Ruman";
+    return 0;
+
+} 
+
+
